d6673a3b831b2502c95b42c535d8656aeefb2cd2
Cordt Voigt <cordt.voigt@t-online.de>
2012-06-13 11:17:07 +0200

    SimpleMap und MapModel PyCharm Neue branch

---
+++ mcc/model/MapSection.py
@@ -0,0 +1,7 @@
+__author__ = 'Cordt'
+
+
+class MapSection:
+    def __init__(self, leftGrid, rightGrid):
+        self.leftGrid = leftGrid
+        self.rightGrid = rightGrid
--- mcc/model/SimpleMap.py
+++ mcc/model/SimpleMap.py
@@ -5,4 +5,8 @@ Created on Tue Jun 05 18:37:12 2012
 @author: Cordt
 """
 
-print "hello"
+class SimpleMap:
+    def __init__(self, nxtPosition, naoPosition, firstMapSection):
+        self.nxtPosition = nxtPosition
+        self.naoPosition = naoPosition
+        self.firstMapSection = firstMapSection
