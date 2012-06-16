17dc805e64554b7a5dfcc5e0bf2d4845684198ed
Cordt <cordt@cordt-ThinkPad-R500.(none)>
2012-06-16 13:48:56 +0200

    fix commit

--- mcc/model/map.py
+++ mcc/model/map.py
@@ -1,74 +1,153 @@
-from mcc.util import utils
+"""
+Module for the map model
 
-class MapModel(object):
+"""
+
+from mcc import utils
+
+class MapModel:
+    """
+    Class for the MapModel. Contains the reference to the first map section
+    which contains the position of the NAO.
+
+    """
 
     def __init__(self):
-        self.__firstMapSection = MapSection()
-        self.__targetPosition = None
+        """
+        Creates a map model
+
+        :return: 'MapModel' with an initial map section ('first_map_section')
+                 and a target position ('target_position')
+        :rtype: MapModel
+
+        """
+        self.__first_map_section = MapSection()
+        self.__target_position = None
 
     @property
-    def firstMapSection(self):
-        return self.__firstMapSection
+    def first_map_section(self):
+        """
+        Getter method for first_map_section
 
-    @firstMapSection.setter
-    def firstMapSection(self, mapSection):
-        if not(type(mapSection) == type(MapSection())):
-            print "Type \"mapSection\" excepted, but",  type(mapSection), " given."
-            return
+        :return: first map section in the map model
+        :rtype: MapSection
+        
+        """
+        return self.__first_map_section
 
-        self.__firstMapSection = mapSection
+    @first_map_section.setter
+    def first_map_section(self, map_section):
+        """
+        Setter method for first_map_section
+
+        :param map_section: map section to replace the current
+        :type map_section: MapSection
+        :raises TypeError: If the type of the arguments is not MapSection
+        
+        """
+        if type(map_section) != type(MapSection()):
+            raise TypeError("Type \"MapSection\" excepted,\
+                             but",  type(map_section), " given.")
+
+        self.__first_map_section = map_section
 
     @property
-    def targetPosition(self):
-        return self.__targetPosition
+    def target_position(self):
+        """
+        Getter method for target_position
 
-    @targetPosition.setter
-    def targetPosition(self, targetPosition):
-        if type(targetPosition) != type(utils.Point(0, 0)):
-            print "Type \"Point\" excepted, but",  type(targetPosition), " given."
-            return
+        :return: position of the target in the map model
+        :rtype: Point
+        
+        """
+        return self.target_position
 
-        self.__targetPosition = targetPosition
+    @target_position.setter
+    def target_position(self, target_position):
+        """
+        Setter method for target_position
 
-    def addMapSection(self, offsetX, offsetY):
-        if (type(offsetX) != type(1)) or (type(offsetY) != type(1)):
-            print "Type \"int\" excepted, but", type(offsetX), ", ", type(offsetY), " given."
-            return
-        if offsetX == 0 and offsetY == 0:
-            print "Can't add MapSection at position (0,0)."
-            return
+        :param target_position: point to replace the current target position
+        :type target_position: Point
+        :raises TypeError: If the type of the arguments is not Point
+        
+        """
+        if type(target_position) != type(utils.Point(0, 0)):
+            raise TypeError("Type \"Point\" excepted, but",
+                            type(target_position), " given.")
 
-        tmpMapSection = self.__firstMapSection
+        self.__target_position = target_position
 
-        if offsetX > 0:
-            for i in range(0, offsetX):
-                tmpMapSection = tmpMapSection.rightGrid
-        elif offsetX < 0:
-            for i in range(0, offsetX, -1):
-                tmpMapSection = tmpMapSection.leftGrid
-        if offsetY > 0:
-            for i in range(0, offsetY):
-                tmpMapSection = tmpMapSection.topGrid
-        elif offsetY < 0:
-            for i in range(0, offsetY, -1):
-                tmpMapSection = tmpMapSection.bottomGrid
+    def add_map_section(self, offset_x, offset_y):
+        """
+        Adds a map section at the position stated with the offset.
+        If there are free spaces (no grids) in the way to the position,
+        where the new grid will be placed, they are automatically created.
 
-        if (offsetX > 0 and tmpMapSection.rightGrid is not None) or\
-           (offsetX < 0 and tmpMapSection.leftGrid is not None) or\
-           (offsetY > 0 and tmpMapSection.topGrid is not None) or\
-           (offsetY < 0 and tmpMapSection.bottomGrid is not None):
-            print "Can't add MapSection with this offset - position is occupied."
-            return
+        :param offset_x: identifies the steps on the x-axis
+        :type offset_x: int
+        :param offset_y: identifies the steps on the y-axis
+        :type offset_y: int
+        :raises TypeError: If the type of the arguments is not integer
+        :raises ValueError: If the offset is (0,0) or already occupied
+        
+        """
+        if (type(offset_x) != type(1)) or (type(offset_y) != type(1)):
+            raise TypeError("Type \"int\" excepted, but", type(offset_x),
+                            ", ", type(offset_y), " given.")
+        if offset_x == 0 and offset_y == 0:
+            raise ValueError("Can't add MapSection at position (0,0).")
 
+        tmp_map_section = self.__first_map_section
+
+        if offset_x > 0:
+            for i in range(0, offset_x):
+                if tmp_map_section.right_grid is None:
+                    tmp_map_section.right_grid = MapSection()
+                tmp_map_section = tmp_map_section.right_grid
+        elif offset_x < 0:
+            for i in range(0, offset_x, -1):
+                if tmp_map_section.left_grid is None:
+                    tmp_map_section.left_grid = MapSection()
+                tmp_map_section = tmp_map_section.left_grid
+        if offset_y > 0:
+            for i in range(0, offset_y):
+                if tmp_map_section.top_grid is None:
+                    tmp_map_section.top_grid = MapSection()
+                tmp_map_section = tmp_map_section.top_grid
+        elif offset_y < 0:
+            for i in range(0, offset_y, -1):
+                if tmp_map_section.bottom_grid is None:
+                    tmp_map_section.bottom_grid = MapSection()
+                tmp_map_section = tmp_map_section.bottom_grid
+
+        if (offset_x > 0 and tmp_map_section.right_grid is not None) or\
+           (offset_x < 0 and tmp_map_section.left_grid is not None) or\
+           (offset_y > 0 and tmp_map_section.top_grid is not None) or\
+           (offset_y < 0 and tmp_map_section.bottom_grid is not None):
+            raise ValueError("Can't add MapSection with this offset - \
+                             position is occupied.")
 
 
 class MapSection(object):
+    """
+    Class for the MapSection. Contains the references to the bordering grids
+    and the grid which is defined by 'GRID_HEIGHT' and 'GRID_WIDTH'.
 
+    """
     def __init__(self):
-        self.__bottomGrid = None
-        self.__leftGrid = None
-        self.__topGrid = None
-        self.__bottomGrid = None
+        """
+        Creates a map section
+
+        :return: 'MapSection' right -, left-, top- and bottom-grid
+                 which are initially all None
+        :rtype: MapSection
+        
+        """
+        self.__right_grid = None
+        self.__left_grid = None
+        self.__top_grid = None
+        self.__bottom_grid = None
 
         self.__GRID_HEIGHT = 10
         self.__GRID_WIDTH = 10
@@ -82,67 +161,143 @@ class MapSection(object):
                 self.__grid[i].append(0)
 
     @property
-    def bottomGrid(self):
-        return self.__bottomGrid
+    def right_grid(self):
+        """
+        Getter method for right_grid
+    
+        :return: the grid which is concatenated at the right of this grid
+        :rtype: MapSection
+        
+        """ 
+        return self.__bottom_grid
 
-    @bottomGrid.setter
-    def bottomGrid(self, bottomGrid):
-        if type(bottomGrid) != type(MapSection()):
-            print "Type \"MapSection\" excepted, but",  type(bottomGrid), " given."
-            return
+    @right_grid.setter
+    def right_grid(self, right_grid):
+        """
+        Setter method for right_grid
 
-        self.__bottomGrid = bottomGrid
+        :param right_grid: grid to replace the reference to the current 
+                           right grid 
+        :type right_grid: MapSection
+        :raises TypeError: If the type of the arguments is not MapSection
+        
+        """
+        if type(right_grid) != type(MapSection()):
+            raise TypeError("Type \"MapSection\" excepted, but",
+                            type(right_grid), " given.")
 
-    @property
-    def leftGrid(self):
-        return self.__leftGrid
-
-    @leftGrid.setter
-    def setLeftGrid(self, leftGrid):
-        if type(leftGrid) != type(MapSection()):
-            print "Type \"MapSection\" excepted, but",  type(leftGrid), " given."
-            return
-
-        self.__leftGrid = leftGrid
-
-    @property
-    def topGrid(self):
-        return self.__topGrid
-
-    @topGrid.setter
-    def setTopGrid(self, topGrid):
-        if type(topGrid) != type(MapSection()):
-            print "Type \"MapSection\" excepted, but",  type(topGrid), " given."
-            return
-
-        self.__topGrid = topGrid
+        self.__right_grid = right_grid
 
     @property
-    def bottomGrid(self):
-        return self.__bottomGrid
+    def left_grid(self):
+        """
+        Getter method for left_grid
+    
+        :return: the grid which is concatenated at the left of this grid
+        :rtype: MapSection
+        
+        """
+        return self.__left_grid
 
-    @bottomGrid.setter
-    def setBottomGrid(self, bottomGrid):
-        if type(bottomGrid) != type(MapSection()):
-            print "Type \"MapSection\" excepted, but",  type(bottomGrid), " given."
-            return
+    @left_grid.setter
+    def left_grid(self, left_grid):
+        """
+        Setter method for left_grid
 
-        self.__bottomGrid = bottomGrid
+        :param left_grid: grid to replace the reference to the current 
+                           left grid 
+        :type left_grid: MapSection
+        :raises TypeError: If the type of the arguments is not MapSection
+        
+        """
+        if type(left_grid) != type(MapSection()):
+            raise TypeError("Type \"MapSection\" excepted, but",
+                            type(left_grid), " given.")
 
-    def updateGrid(self, points):
+        self.__left_grid = left_grid
+
+    @property
+    def top_grid(self):
+        """
+        Getter method for top_grid
+    
+        :return: the grid which is concatenated at the top of this grid
+        :rtype: MapSection
+        
+        """
+        return self.__top_grid
+
+    @top_grid.setter
+    def top_grid(self, top_grid):
+        """
+        Setter method for top_grid
+
+        :param top_grid: grid to replace the reference to the current 
+                           top grid 
+        :type top_grid: MapSection
+        :raises TypeError: If the type of the arguments is not MapSection
+        
+        """
+        if type(top_grid) != type(MapSection()):
+            raise TypeError("Type \"MapSection\" excepted, but",
+                            type(top_grid), " given.")
+
+        self.__top_grid = top_grid
+
+    @property
+    def bottom_grid(self):
+        """
+        Getter method for bottom_grid
+    
+        :return: the grid which is concatenated at the bottom of this grid
+        :rtype: MapSection
+        
+        """
+        return self.__bottom_grid
+
+    @bottom_grid.setter
+    def bottom_grid(self, bottom_grid):
+        """
+        Setter method for bottom_grid
+
+        :param bottom_grid: grid to replace the reference to the current 
+                           bottom grid 
+        :type bottom_grid: MapSection
+        :raises TypeError: If the type of the arguments is not MapSection
+        
+        """
+        if type(bottom_grid) != type(MapSection()):
+            raise TypeError("Type \"MapSection\" excepted, but",
+                            type(bottom_grid), " given.")
+
+        self.__bottom_grid = bottom_grid
+
+    def update_grid(self, points):
+        """
+        Updates the grid by increasing the values of the given points.
+
+        :param points: points that shall be updated
+        :type points: [[int, int]]
+        :raises TypeError: If the type of the arguments is not a list of
+                           integer tuple
+        
+        """
         if type(points) != type([]):
-            print "Type \"List\" excepted, but",  type(points), " given."
-            return
+            raise TypeError("Type \"list\" excepted, but",  type(points),
+                            " given.")
 
         for i in range (0, len(points)):
             if len(points[i]) != 2:
-                print "Wrong format!"
-                return
+                raise TypeError("Wrong format!")
 
         for i in range (0, len(points)):
             self.__grid[points[i][0]][points[i][1]] += 1
 
-    def printGrid(self):
+    def print_grid(self):
+        """
+        Auxiliary function which prints the current grid in the console.
+
+        """
         line = ""
         for i in range(0, self.__GRID_HEIGHT):
             for j in range(0, self.__GRID_WIDTH):
--- mcc/model/test.py
+++ mcc/model/test.py
@@ -1,33 +1,4 @@
-from mcc.model import map
-from mcc.util import utils
-
-mapTest = map.MapModel()
-
-mapSection = mapTest.firstMapSection
-
-mapTest.addMapSection(5, "willi")
-
-mapTest.firstMapSection = 2
-
-mapTest.targetPosition = 2
-
-x_ax = 0
-y_ax = -4
-
-align = {"x": ("rightGrid" if x_ax > 0 else "leftGrid"),
-         "y": ("topGrid" if y_ax > 0 else "bottomGrid")}
-
-print mapSection.__getattribute__(align["x"])
-
-print align["x"]
-print align["y"]
-
 """
-mapSection.printGrid()
+Test module
 
-print "\n"
-
-mapSection.updateGrid([[0, 0], [1, 3], [4, 7]])
-
-mapSection.printGrid()
 """
