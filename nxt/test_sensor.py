7eb86f39f92749ffd37d1c243542abd1a85ab8a0
eax0815 <srodersv@informatik.hu-berlin.de>
2012-06-18 12:00:30 +0200

    deleted:    Explorer.py 	new file:   doku/doku.tex 	renamed:    Appendix+2-LEGO+MINDSTORMS+NXT+Direct+commands.pdf -> man/Appendix+2-LEGO+MINDSTORMS+NXT+Direct+commands.pdf 	renamed:    NXC-Tutorial_DE v2-3.pdf -> man/NXC-Tutorial_DE v2-3.pdf 	renamed:    neu.py -> nxt-python_explorer.py 	deleted:    test_scan.py 	deleted:    test_sensor.py 	deleted:    test_turn.py 	deleted:    ultrasonic_onBtnDown.py

--- nxt/test_sensor.py
+++
@@ -1,8 +0,0 @@
-import nxt.locator
-from nxt.sensor import *
-
-brick = nxt.locator.find_one_brick(host='00:16:53:10:48:E7')
-base_sensor = BaseDigitalSensor(brick, PORT_1, False)
-info = base_sensor.get_sensor_info()
-print "info gelesen ",   str(info)
-
