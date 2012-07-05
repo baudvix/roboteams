c4a6ad2c3e683b6d44449a4ecd14cbcf663546a5
MeWorkstar <poldi87@gmail.com>
2012-07-05 14:43:49 +0200

    handshake entfernt, zurueck angepasst 	modified:   nxt/nxc/explorer.h 	modified:   nxt/nxc/explorer.nxc 	modified:   nxt/nxc/libinfra.h 	modified:   nxt/nxc/libinfra.nxc 	modified:   nxt/nxc/libmov.h 	modified:   nxt/nxc/libmov.nxc

--- nxt/nxc/explorer.h
+++ nxt/nxc/explorer.h
@@ -1,7 +1,17 @@
 #ifndef EXPLORER_H
 #define EXPLORER_H
- #define BT_CONN 1
- #define INBOX 5
- #define OUTBOX 1
 
+ #define BT_CONN 1
+ #define INBOX 5 //device0 5+0
+ #define OUTBOX 1 //device0 1+0
+ #define DEBUG 3
+ int found = 0;
+ 
+ mutex print_mutex;
+ mutex blue_mutex;
+ 
+ int id = 10;
+ 
+ void undo_found(void);
+ 
 #endif
--- nxt/nxc/libinfra.h
+++ nxt/nxc/libinfra.h
@@ -3,7 +3,7 @@
 
 //GLOBALS
 //fuer lcd_print()
- static int lcd = 7;
+ int lcd = 7;
 //fuer splitMsg()
 struct payload{
  int funktion;
@@ -15,9 +15,8 @@ struct message{
  int id;
  payload msg;
 };
-
-
-//fuer queue
+ 
+//fuer normale queue
  int queue1[10];
  int queue2[10];
 
@@ -25,13 +24,14 @@ struct message{
  int aus_index = 0;
  int msg_count = 0;
  mutex queue_mutex;
-
- void BTCheck(int conn);
- message splitMsg(string in);
- void lcd_print(string msg);
  
+ void clean_queue(int &queue1[], int &queue2[]);
  void queueInit(int &queue1[], int &queue2[]);
  bool is_empty(void);
  payload dequeue(int &queue1[], int &queue2[]);
  void enqueue(int &queue1[], int &queue2[], payload elem);
+// normale queue ende 
+
+ message splitMsg(string in);
+ void lcd_print(string msg);
 #endif
