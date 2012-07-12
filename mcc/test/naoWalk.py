d434a5b172db3d428173b10071f0a67e312a9f4a
mhhf <mhhf@freenet.de>
2012-07-12 13:17:23 +0200

    Path find algorithm added

--- mcc/test/naoWalk.py
+++ mcc/test/naoWalk.py
@@ -3,7 +3,18 @@ import random
 
 class NaoWalk(object):
     """
+        PSEUDOCODE:
 
+
+        1. Für n Punkte p die zufällig aus den punkten ausgewählt werden, die Frei sind:
+            - Setze Linien und such dir die kürzeste raus
+            - packe den punkt p auf den Mittelpunkt der Linie mit dem Radius r
+            - prüfe ob der kreis einen schwarzen berreich berrührt. wenn ja -> mache den radius kleiner und teste nochmal
+        2. Verbinde alle punkte, die sich überschneiden.
+        3. Prüfe, ob die überschneidungen groß genug sind. -> füge einen extra punkt in der mitte ein und baue einen radius
+
+        PROBLEME:
+        - wie n(p.length) bestimmen?
     """
 
     def __init__(self, map, free_space, start_position):
@@ -16,12 +27,18 @@ class NaoWalk(object):
         :type start_position: List
         """
 
+
         #TODO map, parent (controller)?
         self.__map = map
         self.__free_space = free_space
         self.__start_position = start_position
 
-        self.__
+        #TestVariables
+        # Dimensionen der Karte
+        self.dimLeft = 1
+        self.dimRight = 1
+        self.dimTop = 1
+        self.dimBottom = 0
 
 
     def run(self):
@@ -35,4 +52,7 @@ class NaoWalk(object):
 
         """
 
+        random_x = random.r
+
         return random.randint(0, len(self.__free_space))
+
