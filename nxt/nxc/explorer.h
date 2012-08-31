dba10cbe20db7f23f25035856a2a560fba5c98c8
MeWorkstar <poldi87@gmail.com>
2012-08-31 18:51:57 +0200

    modified:   nxt/nxc/explorer.h 	modified:   nxt/nxc/explorer.nxc 	modified:   nxt/nxc/libmov.h 	modified:   nxt/nxc/libmov.nxc
            update auf allen.

--- nxt/nxc/explorer.h
+++ nxt/nxc/explorer.h
@@ -1,12 +1,13 @@
 #ifndef EXPLORER_H
 #define EXPLORER_H
 
- #define BT_CONN 1
- #define INBOX 5 //device0 5+0
- #define OUTBOX 1 //device0 1+0
+ #define BT_CONN 3
+ #define INBOX 6
+ #define OUTBOX 2
+ 
  #define DEBUG 3
  int found = 0;
- 
+
  mutex print_mutex;
  mutex blue_mutex;
  
