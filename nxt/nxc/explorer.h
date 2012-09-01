3159982cb1bf4cfd3c8d0b24c2750b2364632905
MeWorkstar <poldi87@gmail.com>
2012-09-01 16:02:29 +0200

    modified:   nxt/nxc/explorer.h <- debug-level 2 (normalerweise 3) 	modified:   nxt/nxc/explorer.nxc <- debuglevel geändert für andere 	ausgaben --> event und target task werden ignoriert.

--- nxt/nxc/explorer.h
+++ nxt/nxc/explorer.h
@@ -1,11 +1,11 @@
 #ifndef EXPLORER_H
 #define EXPLORER_H
 
- #define BT_CONN 3
- #define INBOX 7
- #define OUTBOX 3
+ #define BT_CONN 1
+ #define INBOX 5
+ #define OUTBOX 1
  
- #define DEBUG 3
+ #define DEBUG 2
  int found = 0;
 
  mutex print_mutex;
