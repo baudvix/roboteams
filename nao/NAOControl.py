452e886d8431f3eddad34230a74e51cbc25a10c2
Sebastian Guenther <guenthse@informatik.hu-berlin.de>
2012-09-06 16:09:29 +0200

    NAOControl renamed and should be functional now

---
+++ nao/NAOControl.py
@@ -0,0 +1,66 @@
+from naoqi import ALBroker
+from naoqi import ALProxy
+from naoqi import ALModule
+
+from nao.NaoWalk import *
+from nao.NAOCalibration import *
+
+NAO_IP="nao.local"
+
+
+control = None
+
+class NAOControlModule(ALModule):
+	""" needed to transform to bound methods"""
+
+	def __init__(self, name):
+        ALModule.__init__(self, name)
+        # No need for IP and port here because
+        # we have our Python broker connected to NAOqi broker
+
+    def calibrate(self, color):
+    	try:
+    		calibration = NAOCalibration()
+    		calibration.changeBodyOrientation("init")
+    		calibration.performCalibration(color)
+    	except NXTNotFoundException, e:
+    		raise e
+    	
+
+    def walk(self):
+    	try:
+	    	w = NaoWalk()
+			w.motion.walkInit()
+			w.retrieveBall()
+			w.walkUpToBall()
+			#send message to naoclient, that the nxt has to move, wait until nxt moved
+			self.walkToPosition()
+            self.__setTopCamera()
+		except RedBallLostException, e:
+			# w.retrieveBall()
+			pass
+
+
+
+def main():
+
+	pip=NAO_IP
+	pport=9559
+
+	myBroker = ALBroker("myBroker",
+       "0.0.0.0",   # listen to anyone
+       0,           # find a free port and use it
+       pip,         # parent broker IP
+       pport)       # parent broker port
+
+	global Control
+	Control = NAOControlModule("Control")
+
+	try:
+		while True:
+			sleep(1)
+	except:
+		pass
+
+if __name__ == "__main__":
+	main()
