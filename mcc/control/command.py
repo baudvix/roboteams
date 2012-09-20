06cc7c772454701b2b1894edb885f76215ceb8a6
Sebastian Guenther <guenthse@informatik.hu-berlin.de>
2012-09-20 10:08:14 +0200

    minor changes

--- mcc/control/command.py
+++ mcc/control/command.py
@@ -6,7 +6,7 @@ Those commands are based on amp the twisted Asynchronous Message Protocol.
 """
 
 from twisted.protocols.amp import Command, Integer, String, AmpList, Float
-from nao.NAOCalibration import NXTNotFoundException
+#from nao.NAOCalibration import NXTNotFoundException
 #from nao.NaoWalk import RedBallNotFoundException
 
 
